22c561202a20063ebb7da4a1de966a8075cecb6a
Michael Welker <mikegw224@gmail.com>
2022-04-16 13:03:55 -0700

    troublshooting checkWinner function

--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,6 @@
 const playerX = 'X';
 const playerO = 'O';
+let inGame = true;
 let playerTurn = playerX;
 const tiles = document.querySelectorAll('.col-4');
 const boardState = Array(tiles.length);
@@ -18,29 +19,28 @@ play.addEventListener('click', newGame);
 tiles.forEach((tile) => tile.addEventListener('click', tileClick));
 
 function tileClick(event) {
-    if (screen.classList.contains('alert')){
-        return;
-    }
+    if (inGame){
+        const tile = event.target;
+        const tileNumber = tile.dataset.index;
+        if (tile.innerText != '') {
+            return;
+        }
 
-    const tile = event.target;
-    const tileNumber = tile.dataset.index;
-    if (tile.innerText != '') {
-        return;
-    }
+        if (playerTurn === playerX) {
+            tile.innerText = playerX;
+            boardState[tileNumber -1] = playerX;
+            playerTurn = playerO;
+            turn.innerText = 'Player O Turn';
+            console.log(turn);
 
-    if (playerTurn === playerX) {
-        tile.innerText = playerX;
-        boardState[tileNumber -1] = playerX;
-        playerTurn = playerO;
-        turn.innerText = 'Player O Turn';
-        console.log(turn);
+        } else {
+            tile.innerText = playerO;
+            boardState[tileNumber - 1] = playerO;
+            playerTurn = playerX;
+            turn.innerText = 'Player X Turn';
+            console.log(turn);
+        }
 
-    } else {
-        tile.innerText = playerO;
-        boardState[tileNumber - 1] = playerO;
-        playerTurn = playerX;
-        turn.innerText = 'Player X Turn';
-        console.log(turn);
     }
 
     checkWinner();
@@ -50,16 +50,17 @@ function tileClick(event) {
 
 //array of winning combos & function to check for winner (WIP)
 function checkWinner(){
+    let gameWon = false;
 
     const winningCombinations = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
         [1, 4, 7],
         [2, 5, 8],
-        [3, 6, 9],
-        [1, 5, 9],
-        [3, 5, 7],
+        [0, 4, 8],
+        [2, 4, 6],
     ];
 
 
@@ -69,13 +70,12 @@ function checkWinner(){
         const tileValue2 = boardState[winCombo[1]];
         const tileValue3 = boardState[winCombo[2]];
 
-        if (tileValue1 === '' || tileValue2 === '' || tileValue3 === '') {
-            continue;
-        }
 
-        if(tileValue1 === tileValue2 && tileValue2 === tileValue3) {
-            gameOverBanner(tileValue1);
-            
+        if(tileValue1 != null && tileValue1 === tileValue2 && tileValue1 === tileValue3) {
+            gameWon = true;
+            inGame = false;
+            gameOverBanner(tileValue1, gameWon);
+            break;
         }
     }
 
@@ -85,19 +85,29 @@ function checkWinner(){
 
 
 //game over alert message (WIP) and play button functionality
-function gameOverBanner(winnerText){
-    let text = 'Draw';
-    if (winnerText != null) {
-        text = `Winner is ${winnerText}! Press Play to play again.`
-    }
-    const div = document.createElement('div');
-    div.setAttribute('class', 'alert alert-success');
-    div.setAttribute('role', 'alert');
-    div.setAttribute('id', 'game-over-alert');
-    div.innerText = `${text}`;
-    screen.className = 'visible';
-    screen.appendChild(div);
+function gameOverBanner(winnerText, gameWon){
     
+    if (winnerText != null && gameWon === true) {
+        let text = `Winner is ${winnerText}! Press Play to play again.`;
+        const div = document.createElement('div');
+        div.setAttribute('class', 'alert alert-success');
+        div.setAttribute('role', 'alert');
+        div.setAttribute('id', 'game-over-alert');
+        div.innerText = `${text}`;
+        screen.className = 'visible';
+        screen.appendChild(div);
+    } 
+        
+    if (winnerText != null && gameWon === false) {
+        text = 'Draw! Press Play to play again';
+        const div = document.createElement('div');
+        div.setAttribute('class', 'alert alert-primary');
+        div.setAttribute('role', 'alert');
+        div.setAttribute('id', 'game-over-alert');
+        div.innerText = `${text}`;
+        screen.className = 'visible';
+        screen.appendChild(div);
+    }
 }
 
 function newGame() {
